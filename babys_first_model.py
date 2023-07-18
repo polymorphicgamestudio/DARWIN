aac1179a52ce2277eb3264b59fbc904c20b36849
cabarger <bargercj20@hotmail.com>
2023-07-18 12:42:29 -0700

    working changes

--- babys_first_model.py
+++ babys_first_model.py
@@ -2,47 +2,51 @@
 Simple bigraph character level language model
 NOTE(caleb): Bigraph language models apprently SUCK so don't expect great outputs...
 """
-
+import sys
 import torch
 import torch.nn.functional as F
 from tqdm import tqdm # Progress bars
 
+# Very important splash screen
+sys.stdout.write("\033c") # NOTE(caleb): OS agnostic way to clear screen
+print("""
+ ______   ________   ______    __ __ __    ________  ___   __      
+/_____/\ /_______/\ /_____/\  /_//_//_/\  /_______/\/__/\ /__/\    
+\:::_ \ \\\\::: _  \ \\\\:::_ \ \ \:\\\\:\\\\:\ \ \__.::._\/\::\_\\\\  \ \   
+ \:\ \ \ \\\\::(_)  \ \\\\:(_) ) )_\:\\\\:\\\\:\ \   \::\ \  \:. `-\  \ \  
+  \:\ \ \ \\\\:: __  \ \\\\: __ `\ \\\\:\\\\:\\\\:\ \  _\::\ \__\:. _    \ \ 
+   \:\/.:| |\:.\ \  \ \\\\ \ `\ \ \\\\:\\\\:\\\\:\ \/__\::\__/\\\\. \`-\  \ \\
+    \____/_/ \__\/\__\/ \_\/ \_\/ \_______\/\________\/ \__\/ \__\/ v0.0.1
+""")
+
 mc_path = 'data/clean_McDonald_s_Reviews.txt'
 mc_reviews = open(mc_path, 'r').read().splitlines()
-
-# Unique characters in data set
-chars = sorted(list(set(''.join(mc_reviews))))
+print('Read:', mc_path)
+chars = sorted(list(set(''.join(mc_reviews)))) # Unique characters in data set
 vocab_size = len(chars)
-print(''.join(chars))
+print('Initialized vocab:',''.join(chars))
 
-# Create a mapping from characters to integers
-stoi = { s:i+1 for i,s in enumerate(chars) }
+stoi = { s:i+1 for i,s in enumerate(chars) } # Mapping from characters to integers
 special = '🔥' # NOTE(caleb): Some unique identifier (that isn't in vocab)
+assert(special not in stoi)  
 stoi[special] = 0
 vocab_size += 1
 itos = { i:s for s,i in stoi.items() } # Map back to characters
 encode = lambda s: [stoi[c] for c in s] # Take a string, output list of characters 
 decode = lambda l: ''.join([itos[i] for i in l]) # Take a list of integers, output a string
 
-# Build 'counts' matrix 
+print("Constructing counts matrix: ") 
 N = torch.zeros((vocab_size, vocab_size), dtype=torch.int32)
-for review in tqdm(mc_reviews):
+# TODO(caleb): Investiage why this is taking WAY longer than it should. I have a feeling
+# that some funny buisness is going on inside of the matrix insertion op. 
+for review in tqdm(mc_reviews): 
     chs = [special] + list(review) + [special]
     for ch1, ch2 in zip(chs, chs[1:]):
         ch1_index = stoi[ch1]
         ch2_index = stoi[ch2]
         N[ch1_index, ch2_index] += 1
-
-# Make this script deterministic
-g = torch.Generator().manual_seed(2147483647) 
-
-# Go ahead and precompute probability matrix (probabilty distribution)
-P = (N+1).float() # N + 1 aka model smoothing
-P /= P.sum(1, keepdim=True) # NOTE(caleb): Optimized eqv of:
-# for row_index in range(vocab_size):
-#     P[row_index] /= P[row_index].sum() 
-
-# Create the training set of all the bigrams
+        
+# Create the training set of all the bigrams, where 'xs' are inputs and 'ys' are expected outs.
 xs, ys = [], []
 for review in mc_reviews:
     chs = [special] + list(review) + [special]
@@ -56,27 +60,31 @@ ys = torch.tensor(ys)
 num = xs.nelement()
 print('Number of examples: ', num)
 
+# Make this script deterministic
+g = torch.Generator().manual_seed(2147483647) 
 # Randomly initialize vocab_size neurons' weights. Each neuron receives vocab_size inputs
 W = torch.randn((vocab_size, vocab_size), generator=g, requires_grad=True)
 
-# Gradient descent
-print("GRADIENT DESCENT (this might take a while):")
+# Gradient based optimization 
+print("TRAIN (gradient descent this might take a while):")
 for k in tqdm(range(5)):
     # Forward pass
     xenc = F.one_hot(xs, num_classes=vocab_size).float() # Input to the neural network: one_hot encoding
     logits = xenc @ W # Predict log-counts
     counts = logits.exp() # Something that looks like counts (N matrix)
+    # See softmax https://en.wikipedia.org/wiki/Softmax_function
     probs = counts / counts.sum(1, keepdims=True) # Probabilities for next character
     loss = -probs[torch.arange(len(xs)), ys].log().mean()
+    # print(loss.item())
+    
     # Backward pass
     W.grad = None # Set gradient to zero
     loss.backward()
+    
     # Update
-    W.data += -10 * W.grad
+    W.data += -100 * W.grad
     
-    # print(loss.item())
 
-    
 for i in range(10): # Write 10 reviews
     out = []
     ix = 0
@@ -94,7 +102,7 @@ for i in range(10): # Write 10 reviews
     print(''.join(out)) 
 
     
-# Compute negative log likelihood 
+# Compute average negative log likelihood (aka loss)
 # NOTE(caleb): This single number summarizes model quality (lower is better)
 log_likelihood = 0.0
 n = 0
@@ -112,4 +120,4 @@ for review in mc_reviews[:1]: # nll of this model producing the first review
 print(f'{log_likelihood=}')
 nll = -log_likelihood
 print(f'{nll=}')
-print(f'{nll/n}')         
+print(f'{nll/n}') # NOTE(caleb): Loss
