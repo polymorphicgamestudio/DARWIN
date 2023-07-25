bdd5aa6a98443d8ddc5283c7e3c3b3b5f0ed94c4
cabarger <bargercj20@hotmail.com>
2023-07-25 13:09:06 -0700

    lag bs

--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,73 @@ const rand = std.rand;
 const data_path = "data/tiny_shakes.txt";
 const block_length = 3;
 const feature_length = 4;
+const hidden_layer_neuron_count = 10;
+
+fn HeapVector(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        data: []T,
+        dim: usize,
+        ally: mem.Allocator,
+
+        pub fn initPreheated(ally: mem.Allocator, dim: usize) !HeapVector(T) {
+            var result = Self{
+                .data = undefined,
+                .dim = dim,
+                .ally = ally,
+            };
+            result.data = try ally.alloc(T, dim);
+            return result;
+        }
+
+        pub fn deinit(self: *Self) void {
+            self.ally.free(self.data);
+        }
+    };
+}
+
+fn HeapMatrix(comptime T: type) type {
+    return struct {
+        const Self = @This();
+
+        data: []T,
+        m: usize,
+        n: usize,
+        ally: mem.Allocator,
+
+        pub fn initPreheated(ally: mem.Allocator, m: usize, n: usize) !HeapMatrix(T) {
+            var result = Self{
+                .data = undefined,
+                .m = m,
+                .n = n,
+                .ally = ally,
+            };
+            result.data = try ally.alloc(T, m * n);
+            return result;
+        }
+
+        pub fn deinit(self: *Self) void {
+            self.ally.free(self.data);
+        }
+
+        pub fn multHeapVector(self: Self, v: HeapVector(T)) !HeapVector(T) {
+            var result = try HeapVector(T).initPreheated(self.ally, self.m);
+            var row_index: usize = 0;
+            while (row_index < self.m) : (row_index += 1) {
+                var col_index: usize = 0;
+                while (col_index < self.n) : (col_index += 1)
+                    result.data[row_index] += self.data[row_index * self.n + col_index] * v.data[row_index];
+            }
+            return result;
+        }
+    };
+}
+
+fn matVecMult(comptime T: type, mat: []T, vec: T) void {
+    _ = mat;
+    _ = vec;
+}
 
 fn buildDataset(
     data_file: std.fs.File,
@@ -55,7 +122,7 @@ pub fn main() !void {
     const rng = xoshi.random();
 
     var vocab = std.AutoHashMap(u64, usize).init(arena);
-    var word_id = @as(usize, 1);
+    var vocab_count = @as(usize, 1);
     const data_file = try std.fs.cwd().openFile(data_path, .{});
     defer data_file.close();
 
@@ -78,28 +145,36 @@ pub fn main() !void {
         while (tok != null) : (tok = word_toks.next()) {
             const k = hash_map.hashString(tok.?);
             if (vocab.contains(k)) continue;
-            try vocab.putNoClobber(k, word_id);
-            word_id += 1;
+            try vocab.putNoClobber(k, vocab_count);
+            vocab_count += 1;
         }
     }
 
-    // Dataset(s)
     var inputs = std.ArrayList(@Vector(block_length, usize)).init(arena);
     var outputs = std.ArrayList(usize).init(arena);
     try buildDataset(data_file, vocab, &inputs, &outputs);
 
-    // Build context matrix
-    const row_count = word_id;
-    var context_matrix = try arena.alloc(@Vector(feature_length, f32), row_count);
+    var context_matrix = try arena.alloc(@Vector(feature_length, f32), vocab_count);
     for (context_matrix) |*row| {
         var feature_index = @as(usize, 0);
-        while (feature_index < feature_index)
-            row[feature_index] = rng.floatNorm(f32);
+        while (feature_index < feature_length) : (feature_index += 1)
+            row[feature_index] = rng.float(f32);
     }
-    std.debug.print("Contstructed context matrix: {d}kb\n", .{@sizeOf(@Vector(feature_length, f32)) * row_count / 1024});
+    std.debug.print("Contstructed context matrix: {d}kb\n", .{@sizeOf(@Vector(feature_length, f32)) * vocab_count / 1024});
 
-    // Embeddings
-    // var emb = try arena.alloc(@Vector(feature_length, f32), row_count);
+    var output_layer_matrix = try HeapMatrix(f32).initPreheated(arena, vocab_count, hidden_layer_neuron_count);
+    var output_layer_bias_vector = try HeapVector(f32).initPreheated(arena, vocab_count);
+
+    _ = try output_layer_matrix.multHeapVector(output_layer_bias_vector);
+
+    var hidden_layer_matrix = try HeapMatrix(f32).initPreheated(arena, hidden_layer_neuron_count, feature_length * block_length);
+    var hidden_layer_bias_vector = try HeapVector(f32).initPreheated(arena, hidden_layer_neuron_count);
+    _ = hidden_layer_matrix;
+    _ = hidden_layer_bias_vector;
+    var catd_feature_vector: @Vector(feature_length * block_length, f32) = undefined; // NOTE(caleb): aka the activation layer.
+    _ = catd_feature_vector;
+
+    // Multiply cated_feature_vector with hidden layer matrix
 
     // for (outputs.items, 0..) |expected, output_index|
     //     std.debug.print("{any} => {d}\n", .{ inputs.items[output_index], expected });
