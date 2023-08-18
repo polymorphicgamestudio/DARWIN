58e9492929a0df94e9cc1ca6e9025f0efd5fbd0e
cabarger <bargercj20@hotmail.com>
2023-08-18 11:20:15 -0700

    remove extra linkc/cpp

--- build.zig
+++ build.zig
@@ -6,31 +6,29 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "DARWIN",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
+    // llama library -----------------------------------------------------------------------------
     const llama = b.addStaticLibrary(.{
         .name = "llama",
         .target = target,
         .optimize = optimize,
     });
     llama.addIncludePath(.{ .path = llama_path });
-    // llama.addIncludePath(.{ .path = llama_path ++ "/examples" });
-    llama.linkLibCpp();
-    llama.linkLibC();
     llama.addCSourceFiles(&.{
         llama_path ++ "/ggml.c",
         llama_path ++ "/ggml-alloc.c",
     }, &.{"-std=c11"});
     llama.addCSourceFiles(&.{llama_path ++ "/llama.cpp"}, &.{"-std=c++11"});
     if (target.isWindows()) llama.want_lto = false;
+    llama.linkLibCpp();
+    llama.linkLibC();
 
-    exe.linkLibC();
-    exe.linkLibCpp();
+    // main.zig ----------------------------------------------------------------------------------
+    const exe = b.addExecutable(.{
+        .name = "DARWIN",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
     exe.addIncludePath(.{ .path = llama_path });
     exe.linkLibrary(llama);
 
